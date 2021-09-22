5feac29ed2ce977d9de09ac3827c3c2ebd91846c
maxafanasiev <maxolololo@gmail.com>
2021-09-22 12:39:31 +0300

    Ok!

---
+++ Block/RevBlock2.php
@@ -0,0 +1,61 @@
+<?php
+namespace Perspective\ReviewProduct\Block;
+
+class RevBlock2 extends \Magento\Framework\View\Element\Template
+{
+    protected $productCollectionFactory;
+    protected $productImageHelper;
+    protected $categoryFactory;
+    protected $productRepository;
+    protected $reviewCollectionFactory;
+
+    public function __construct(
+        \Magento\Catalog\Helper\Image                                           $productImageHelper,
+        \Perspective\ReviewProduct\Model\ResourceModel\Review\CollectionFactory $reviewCollectionFactory,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory          $productCollectionFactory,
+        \Magento\Catalog\Model\CategoryFactory                                  $categoryFactory,
+        \Magento\Catalog\Model\ProductRepository                                $productRepository,
+        \Magento\Backend\Block\Template\Context                                 $context,
+        array                                                                   $data = []
+    )
+    {
+        $this->productImageHelper = $productImageHelper;
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->reviewCollectionFactory = $reviewCollectionFactory;
+        $this->categoryFactory = $categoryFactory;
+        $this->productRepository = $productRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getImageUrl($product, $imageId, $attributes = [])
+    {
+        return $this->productImageHelper->init($product, $imageId, $attributes)->getUrl();
+    }
+
+    public function getProductCollection()
+    {
+        $category = $this->getCategoryId();
+        $collection = $this->productCollectionFactory->create();
+        $collection->addAttributeToSelect('*')->addCategoriesFilter(['eq' => $category]);
+        return $collection;
+    }
+
+    public function getReviewCollectionFromCategory()
+    {
+        $category = $this->getCategoryId();
+        $collection = $this->reviewCollectionFactory->create();
+        $collection->addFieldToSelect('*')->addFieldToFilter('category_id', ['eq' => $category]);
+        return $collection;
+    }
+
+    public function getProductById($id)
+    {
+        return $this->productRepository->getById($id);
+    }
+
+    private function getCategoryId()
+    {
+        return [19];
+    }
+
+}
